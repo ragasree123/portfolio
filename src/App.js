e7b55e3c264fc6f6af5ecb8df8c9840e62ff12e1
ragasree123 <51991653+ragasree123@users.noreply.github.com>
2025-06-26 02:33:36 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Navbar from './react-tailwind-portfolio/src/components/Navbar';
+import Navbar from './src/components/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';

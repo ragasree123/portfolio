96c1471eb54e57f8e2a7f5cd155cfb5b2693fc9c
ragasree123 <51991653+ragasree123@users.noreply.github.com>
2025-06-26 11:52:24 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Navbar from './src/components/Navbar';
+import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
